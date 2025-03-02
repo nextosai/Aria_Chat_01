66fb7e0e627b01d95a4342cdd84e9e9748f01a59
Guile Lindroth <guile@nextos.ai>
2025-03-01 21:12:34 -0300

    Updates

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
           <li>
             <a
               onClick={() => {
-                window.open("https://docs.simli.com/introduction");
+                window.open("https://www.realbotix.com");
               }}
               className="block cursor-pointer py-2 px-3 text-gray-900 rounded   md:border-0 md:p-0 dark:text-white hover:underline    "
             >
@@ -16,7 +16,7 @@ export default function Navbar() {
           <li>
             <a
               onClick={() => {
-                window.open("https://simli.com");
+                window.open("https://www.realbotix.com");
               }}
               className="block cursor-pointer py-2 px-3 text-gray-900 rounded   md:border-0  md:p-0 dark:text-white hover:underline    "
             >
@@ -26,7 +26,7 @@ export default function Navbar() {
           <li>
             <a
               onClick={() => {
-                window.open("https://discord.gg/yQx49zNF4d");
+                window.open("https://www.realbotix.com");
               }}
               className="block cursor-pointer py-2 px-3 text-gray-900 rounded   md:border-0  md:p-0 dark:text-white hover:underline    "
             >
